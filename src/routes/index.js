eecbe8ef7d0e46a1d737a07a8fd16a408f09cdc4
klebersilvars <kleber.600.l@gmail.com>
2024-05-06 11:08:28 -0300

    Instalação do Drawer

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,50 +1,20 @@
 
 import React from 'react';
 
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import {createDrawerNavigator} from '@react-navigation/drawer'
 import Sobre from '../pages/Sobre';
 import Feather from 'react-native-vector-icons/Feather'
 import StackRoute from './stackRoute';
 
-const Tab = createBottomTabNavigator()
+const Drawer = createDrawerNavigator()
 
 export default function Routes() {
 
   
   return (
-
-        <Tab.Navigator screenOptions={{
-          tabBarHideOnKeyboard: false,
-          tabBarShowLabel: false,
-          tabBarActiveTintColor: 'white',
-
-          tabBarStyle: {
-            backgroundColor: '#252525'
-          }
-        }}>
-
-          <Tab.Screen
-           name='HomeStack' 
-           component={StackRoute}
-
-           options={{
-            headerShown: false,
-            tabBarIcon: ({color, size})=> {
-              return <Feather name='home' color={color} size={size}/>
-            }
-           }}
-           />
-
-          <Tab.Screen 
-          name='Sobre' 
-          component={Sobre}
-          options={{
-            headerShown: false,
-            tabBarIcon: ({color, size})=> {
-              return <Feather name='file-text' color={color} size={size}/>
-            }
-           }}
-          />
-        </Tab.Navigator>
+    <Drawer.Navigator>
+        <Drawer.Screen name="HomeStack" component={StackRoute}/>
+    </Drawer.Navigator>
+      
   );
 }
